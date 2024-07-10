6f97e9fdb48cf937e98a8ef136315b816d5a7fdf
vp-174 <137095641+vp-174@users.noreply.github.com>
2024-07-10 14:38:34 +0500

    +

--- main2.py
+++ main2.py
@@ -8,20 +8,20 @@ from PySide6.QtWidgets import *
 import winsound
 import configparser
 
-access_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ5V29landCTmxROWtQVEpFZnFpVzRrc181Mk1KTWkwUHl2RHNKNlgzdlFZIn0.eyJleHAiOjE3MjA1NTc5ODQsImlhdCI6MTcyMDUyMTk4NCwianRpIjoiOTZlZDU3MmItYWZiMy00YzczLWI3ZTgtMjY1YTM4ZDRhNDFhIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLnNiZXJjbGFzcy5ydS9hdXRoL3JlYWxtcy9FZHVQb3dlcktleWNsb2FrIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjIzYzBlN2MxLWY0ZTQtNDNlNC05ZTRjLTFiOGI3OGE3ODZlMCIsInR5cCI6IkJlYXJlciIsImF6cCI6InMyMS1vcGVuLWFwaSIsInNlc3Npb25fc3RhdGUiOiJlYjgwN2EyOC0wMjZhLTQwMTktOGRhMS0wMjgxMmEzYmY4M2QiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vZWR1LjIxLXNjaG9vbC5ydSJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1lZHVwb3dlcmtleWNsb2FrIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJ1c2VyX2lkIjoiMDJiODk3NWQtNGE3OC00ZWE0LWExZDktNjg3Mzk4NWU3MmI4IiwibmFtZSI6Ikdlb2ZmcmV5IEFubmljZSIsImF1dGhfdHlwZV9jb2RlIjoiZGVmYXVsdCIsInByZWZlcnJlZF91c2VybmFtZSI6Imdlb2ZmcmVhQHN0dWRlbnQuMjEtc2Nob29sLnJ1IiwiZ2l2ZW5fbmFtZSI6Ikdlb2ZmcmV5IiwiZmFtaWx5X25hbWUiOiJBbm5pY2UiLCJlbWFpbCI6Imdlb2ZmcmVhQHN0dWRlbnQuMjEtc2Nob29sLnJ1In0.Cg3UvMDLeUGO1pGkJ71GQU2kEmAZt2Rtklm7gPoWYvdB3R-CncJPeAbAwcdH3p7jZ2Zn7iWkJKiKXP8VKMDwIy9wYJkLcdTT4n_X2TyFj3PIRecvsLVLKnpkxYb8qHaQGUHHs4nj_01Vq5Go39S23oZfVk5swewkMd2lQk-5Y7hSf7CL98S4QFy1wln_p3KdrsbG5yd8SkX_DXXkqAzgmiG-4eGjot0_mnluoQJ1Zm9Wl8NoQn6tArutUugQHlprMPOLyC4ahWRdUvLauL3tCArN3uBzh6kZrJABLVC0GgDITsFTLJokyDpGG4-9PMUVbE3CDdTlWpC4ZA4iyeeanA"
-# def get_access_token(username, password):
-#     url = 'https://auth.sberclass.ru/auth/realms/EduPowerKeycloak/protocol/openid-connect/token'
-#     payload = {
-#         "username": username,
-#         "password": password,
-#         "grant_type": "password",
-#         "client_id": "s21-open-api"
-#     }
-#     response = requests.post(url, data=payload)
-#     if response.status_code == 200:
-#         return response.json().get("access_token")
-#     else:
-#         return None
+# access_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ5V29landCTmxROWtQVEpFZnFpVzRrc181Mk1KTWkwUHl2RHNKNlgzdlFZIn0.eyJleHAiOjE3MjA1NTc5ODQsImlhdCI6MTcyMDUyMTk4NCwianRpIjoiOTZlZDU3MmItYWZiMy00YzczLWI3ZTgtMjY1YTM4ZDRhNDFhIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLnNiZXJjbGFzcy5ydS9hdXRoL3JlYWxtcy9FZHVQb3dlcktleWNsb2FrIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjIzYzBlN2MxLWY0ZTQtNDNlNC05ZTRjLTFiOGI3OGE3ODZlMCIsInR5cCI6IkJlYXJlciIsImF6cCI6InMyMS1vcGVuLWFwaSIsInNlc3Npb25fc3RhdGUiOiJlYjgwN2EyOC0wMjZhLTQwMTktOGRhMS0wMjgxMmEzYmY4M2QiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vZWR1LjIxLXNjaG9vbC5ydSJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1lZHVwb3dlcmtleWNsb2FrIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJ1c2VyX2lkIjoiMDJiODk3NWQtNGE3OC00ZWE0LWExZDktNjg3Mzk4NWU3MmI4IiwibmFtZSI6Ikdlb2ZmcmV5IEFubmljZSIsImF1dGhfdHlwZV9jb2RlIjoiZGVmYXVsdCIsInByZWZlcnJlZF91c2VybmFtZSI6Imdlb2ZmcmVhQHN0dWRlbnQuMjEtc2Nob29sLnJ1IiwiZ2l2ZW5fbmFtZSI6Ikdlb2ZmcmV5IiwiZmFtaWx5X25hbWUiOiJBbm5pY2UiLCJlbWFpbCI6Imdlb2ZmcmVhQHN0dWRlbnQuMjEtc2Nob29sLnJ1In0.Cg3UvMDLeUGO1pGkJ71GQU2kEmAZt2Rtklm7gPoWYvdB3R-CncJPeAbAwcdH3p7jZ2Zn7iWkJKiKXP8VKMDwIy9wYJkLcdTT4n_X2TyFj3PIRecvsLVLKnpkxYb8qHaQGUHHs4nj_01Vq5Go39S23oZfVk5swewkMd2lQk-5Y7hSf7CL98S4QFy1wln_p3KdrsbG5yd8SkX_DXXkqAzgmiG-4eGjot0_mnluoQJ1Zm9Wl8NoQn6tArutUugQHlprMPOLyC4ahWRdUvLauL3tCArN3uBzh6kZrJABLVC0GgDITsFTLJokyDpGG4-9PMUVbE3CDdTlWpC4ZA4iyeeanA"
+def get_access_token(username, password):
+    url = 'https://auth.sberclass.ru/auth/realms/EduPowerKeycloak/protocol/openid-connect/token'
+    payload = {
+        "username": username,
+        "password": password,
+        "grant_type": "password",
+        "client_id": "s21-open-api"
+    }
+    response = requests.post(url, data=payload)
+    if response.status_code == 200:
+        return response.json().get("access_token")
+    else:
+        return None
 
 def save_token_to_file(token):
     config = configparser.ConfigParser()
@@ -58,13 +58,12 @@ def check_auth_and_notify():
         print(f"Error: {e}")
 
 def check_auth(access_token):
-    url1 = 'https://edu-api.21-school.ru/services/21-school/api/v1/events?from=2024-01-23T00%3A00%3A00Z&to=2024-01-24T00%3A00%3A00Z&type=TEST&limit=50&offset=0'
-    url2 = 'https://edu-api.21-school.ru/services/21-school/api/v1/events?from=2024-07-09T00%3A00%3A00Z&to=2024-07-11T00%3A00%3A00Z&limit=50&offset=0'
+    url = 'https://edu-api.21-school.ru/services/21-school/api/v1/events?from=2024-07-09T00%3A00%3A00Z&to=2024-07-11T00%3A00%3A00Z&limit=50&offset=0'
     headers = {
         'accept': 'application/json',
         'Authorization': f'Bearer {access_token}'
     }
-    response = requests.get(url2, headers=headers)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         print(response.json().get("events")) # здесь приходит event
         return True
